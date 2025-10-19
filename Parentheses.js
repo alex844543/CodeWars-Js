dc2d1b23d9d71b65eb8c49c92dde1b2175c5e896
Amritansh Roy <f23701837@gmail.com>
2025-10-19 21:23:17 +0530

    Implement validParentheses function

---
+++ Parentheses.js
@@ -0,0 +1,14 @@
+function validParentheses(parenStr) {
+  let curr = 0;
+  for(let i = 0; i < parenStr.length; i++) {
+    if (parenStr[i] === '(') {
+      curr++;
+    } else {
+      curr--;
+    }
+    if (curr < 0) {
+      return false;
+    }
+  }
+  return curr === 0;
+}
