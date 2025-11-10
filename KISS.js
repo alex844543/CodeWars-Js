67648e41d8868d1bc7c385fba03b0c86d5b00441
Amritansh Roy <f23701837@gmail.com>
2025-11-10 18:35:47 +0530

    Add isKiss function to evaluate word lengths

---
+++ KISS.js
@@ -0,0 +1,18 @@
+function isKiss( words ){
+let split = words.split(" ");
+let totalCount = split.length;
+  
+  for(let i = 0; i<split.length;i++){
+    if(split[i].length > totalCount){
+ 
+return "Keep It Simple Stupid"
+    }
+    
+  }
+  
+  return "Good work Joe!"
+
+  
+  
+
+}
