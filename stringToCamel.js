020c424aa105624f42fefffaf4bae531cc772581
Amritansh Roy <f23701837@gmail.com>
2026-02-11 18:28:04 +0530

    Create stringToCamel.js

---
+++ stringToCamel.js
@@ -0,0 +1,13 @@
+function toCamelCase(str){
+  let words = str.split(/[-_]/)
+  
+  let result = words[0]
+  
+  for(let i = 1 ; i<words.length;i++){
+    
+    result+= words[i][0].toUpperCase() + words[i].slice(1)
+    
+  }
+  
+ return result
+}
