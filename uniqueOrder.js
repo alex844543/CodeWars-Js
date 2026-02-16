f174430bcbdef205e8ad822eb9a014bcc90fa801
Amritansh Roy <f23701837@gmail.com>
2026-02-16 14:03:34 +0530

    Add uniqueInOrder function to remove duplicates
    Implement uniqueInOrder function to filter consecutive duplicates from iterable.

---
+++ uniqueOrder.js
@@ -0,0 +1,13 @@
+var uniqueInOrder=function(iterable){
+
+let result = []
+
+for(let i = 0 ; i<iterable.length;i++){
+if(iterable[i] !== iterable[i-1] ){
+  result.push(iterable[i])
+}
+}
+  
+  return result
+
+}
