e38cbc832fd818c09ae0b707f76d2d43c32f3bc3
Amritansh Roy <f23701837@gmail.com>
2026-03-05 14:49:58 +0530

    Create highRank.js

---
+++ highRank.js
@@ -0,0 +1,23 @@
+function highestRank(arr){
+
+let map = {}
+
+for(let a of arr){
+map[a] = (map[a] || 0) + 1
+}
+  let max = 0
+  let result = 0 
+  
+  
+  for(let a in map){
+    if(map[a] > max){
+      max = map[a]
+      result = Number(a)
+    }else if(map[a] === max && Number(a) > result){
+      result = Number(a)
+}
+  }
+  
+  return result 
+
+}
