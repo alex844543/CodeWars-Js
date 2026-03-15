06181680d9ced358a6a2f2e42630d0f112b484bc
Amritansh Roy <f23701837@gmail.com>
2026-03-15 14:07:40 +0530

    Create aretheySame.js

---
+++ aretheySame.js
@@ -0,0 +1,14 @@
+function comp(array1, array2){
+if(!array1 || !array2) return false
+  
+  let arr1 = array1.map(x => x*x).sort((a,b) => a-b)
+  let arr2 = [...array2].sort((a,b) => a-b);
+  
+  for(let i = 0; i<arr1.length ; i++){
+    if(arr1[i] !== arr2[i]){
+      return false
+    }
+  }
+  return true
+
+}
