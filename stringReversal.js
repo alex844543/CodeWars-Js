4f0f9bde445f44722170238775323c016322f947
Amritansh Roy <f23701837@gmail.com>
2026-03-21 20:11:09 +0530

    Create stringReversal.js

---
+++ stringReversal.js
@@ -0,0 +1,13 @@
+function solve(st, a, b){
+ 
+  let right = st.slice(0,a)
+  let middle = st.slice(a,b+1)
+  let left = st.slice(b+1)
+  
+  let reversed = middle.split("").reverse().join("")
+  
+  return right + reversed + left
+  
+  
+}
+  
