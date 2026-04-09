68bd7c429cb5f47f680e9d00befc8b80971a65fd
Amritansh Roy <f23701837@gmail.com>
2026-04-09 19:23:27 +0530

    Add amidakuji function for array processing
    Implement amidakuji function to process an array of strings representing connections.

---
+++ amidakuji.js
@@ -0,0 +1,20 @@
+function amidakuji(ar){
+	
+  
+  let n = ar[0].length + 1
+  
+  let newArr = Array.from({length: n}, (_, i) => i);
+  for(let row of ar){
+    for(let i = 0 ; i < row.length;i++){
+      if(row[i]  === '1'){
+        let temp = newArr[i];
+        newArr[i] = newArr[i+1]
+        newArr[i+1] = temp
+      }
+    }
+  }
+  
+  return newArr
+  
+  
+}
