7fc92fded9fdb89418b171c0d9ff5c3baab0f1a1
Amritansh Roy <f23701837@gmail.com>
2026-04-27 19:32:37 +0530

    Add helpZoom function for key validation
    Implement helpZoom function to check if the input key is a perfect square and a palindrome.

---
+++ helpZoom.js
@@ -0,0 +1,15 @@
+function helpZoom(key){
+  const n = Math.sqrt(key.length)
+  
+  if (!Number.isInteger(n)) return "No"
+
+  
+  for(let i  = 0 ; i < key.length/2 ; i++){
+
+    if(key[i] !== key[key.length - (i+1)])
+      return "No"
+  }
+  
+  return "Yes"
+  
+}
