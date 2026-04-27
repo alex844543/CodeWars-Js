8fe882c6919d07b619985b5058442aba5d8c99ee
Amritansh Roy <f23701837@gmail.com>
2026-04-27 19:31:00 +0530

    Add wrappingPaper function to calculate total wrapping

---
+++ wrappingPaper.js
@@ -0,0 +1,16 @@
+function wrappingPaper(boxes) {
+  
+  let total = 0
+  
+  for(let i = 0 ; i  < boxes.length ; i++){
+    let [l,w,h] = boxes[i]
+    
+    let surface  = 2 * (l*w + w*h + h*l)
+    let smallest  = Math.min(l*w,w*h,h*l)
+    total += smallest + surface
+  }
+  
+  return total
+  
+  
+}
