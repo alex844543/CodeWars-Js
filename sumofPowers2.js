e2b5dfe5ad0820064cdf5a5a6bc13c0b053c7c83
Amritansh Roy <f23701837@gmail.com>
2026-04-28 20:36:13 +0530

    Add function to calculate powers of 2 from sum
    Implement a function to return powers of 2 from the sum of input n.

---
+++ sumofPowers2.js
@@ -0,0 +1,31 @@
+/ return an array of numbers (that are a power of 2)
+// for which the input "n" is the sum
+const powers = n => {
+  
+  const binary = n.toString(2).split("").map(Number);
+
+  let result = [];
+  
+  for(let i = binary.length -1 ; i >=0 ; i--){
+    let power = 1;
+    
+    if(binary[i] === 1){
+      power = 2**(binary.length - i - 1)
+                      result.push(power)
+
+
+    }
+
+      
+      else {
+        continue;
+      }
+    
+    
+  }
+  
+  return result.sort((a,b) => a-b)
+  
+
+
+};
