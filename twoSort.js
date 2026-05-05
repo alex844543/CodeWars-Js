418dba256efb1347a9bd8069756f04848a5c5167
Amritansh Roy <f23701837@gmail.com>
2026-05-05 19:43:45 +0530

    Add twoSort function to format first sorted string
    Implement function to sort array and format first element.

---
+++ twoSort.js
@@ -0,0 +1,18 @@
+function twoSort(s) {
+
+  let sorted = s.sort()
+  
+  let first = sorted[0]
+  let result = ''
+  for(let i = 0 ; i < first.length ; i++){
+    
+    if(i !== first.length-1)
+      result+=first[i] + "***"
+    else 
+    result+=first[i]
+  }
+  
+  return result
+  
+  
+}
