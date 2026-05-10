c47f26ab51e92a50399d87f9eca18bc847629f1c
Amritansh Roy <f23701837@gmail.com>
2026-05-10 13:43:19 +0530

    Fix formatting issue in evenLast.js

---
+++ evenLast.js
@@ -0,0 +1,18 @@
+function evenLast(numbers) {
+
+  if(numbers.length === 0) return 0
+  
+  
+let last = numbers[numbers.length-1]
+let sum = 0
+for(let i = 0 ; i < numbers.length ; i++){
+  if(i%2 === 0){
+    sum+=numbers[i]
+  }
+}
+  
+  last*=sum
+  
+  return last
+
+}
