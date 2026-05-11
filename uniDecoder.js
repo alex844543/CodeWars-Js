59a9a73a0d54c15633dd033f5e0ae4a25db74fbe
Amritansh Roy <f23701837@gmail.com>
2026-05-11 20:06:01 +0530

    yhhhhh
    jhhnn

--- uniDecoder.js
+++ uniDecoder.js
@@ -2,6 +2,7 @@ function uniDecoder(...nums){
   let arr = [...nums]
   let str = ''
   
+  
   for(let i = 0 ;  i <arr.length ; i++){
 if(typeof arr[i] === 'number'){
   let c = String.fromCharCode(arr[i])
