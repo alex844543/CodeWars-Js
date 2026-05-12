a4942579d2260dcdcb5f71a98fa524f808a21e7f
Amritansh Roy <f23701837@gmail.com>
2026-05-12 19:14:08 +0530

    Add findCharacters function to count character frequency
    Implement function to find characters with minimum frequency in a matrix string.

---
+++ findCharacters.js
@@ -0,0 +1,29 @@
+function findCharacters(matrix){
+
+  let split = matrix.split("\n")
+  
+  let count = {}
+  
+  
+  for(let arr of split){
+    for(let c of arr){
+       count[c]  = (count[c] || 0) + 1;
+    }
+  }
+  
+let order = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+let arrr = Object.keys(count).sort((a,b)=>
+  count[a] - count[b] || order.indexOf(a) - order.indexOf(b)
+)
+let min = count[arrr[0]]
+let result =''
+
+for(let c of arrr){
+  if(count[c] == min) 
+    result+=c
+}
+  
+  return result
+  
+}
