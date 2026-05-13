b642b57f9a6cef81ff3fd945532bc82745496de3
Amritansh Roy <f23701837@gmail.com>
2026-05-13 18:43:16 +0530

    Add hydrate function for water glass counting
    Implement hydrate function to count glasses of water

---
+++ hydrate.js
@@ -0,0 +1,13 @@
+function hydrate(s) {
+
+  let ans = 0
+  
+for(let i = 0 ; i < s.length ; i++){
+  if(s[i] >= '0' && s[i]<='9'){
+    ans+=Number(s[i])
+  }
+}
+  
+  return  ans==1 ? `${ans} glass of water` : `${ans} glasses of water`
+
+}
