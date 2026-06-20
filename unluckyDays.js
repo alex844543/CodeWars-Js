9bca91ab72471180edb7e1df6ead799aacbab88b
Amritansh Roy <f23701837@gmail.com>
2026-06-20 20:12:02 +0530

    Implement function to count unlucky days in a year

---
+++ unluckyDays.js
@@ -0,0 +1,16 @@
+function unluckyDays(year){
+
+  let count = 0
+  
+for(let i = 0 ; i <=11 ; i++){
+  let date = new Date(year,i,13)
+  
+  if(date.getDay()=== 5){
+    count++
+  }
+}
+  
+  return count
+
+
+}
