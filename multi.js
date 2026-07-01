bb8bca19f3d5295ed3aae9b603b7dc32d8e07d89
Amritansh Roy <f23701837@gmail.com>
2026-07-01 19:04:40 +0530

    grgrdgdr

---
+++ multi.js
@@ -0,0 +1,11 @@
+function multi(arr) {
+  return arr.reduce((a,b) => a * b , 1)
+}
+function add(arr) {
+    return arr.reduce((a,b) => a + b , 0)
+
+}
+function reverse(str) {
+const reversed = str.split('').reverse().join('');
+  return reversed
+}
