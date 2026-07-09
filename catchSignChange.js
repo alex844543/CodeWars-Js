8e432bfd60725ce447d061ffce38917762d11b49
Amritansh Roy <f23701837@gmail.com>
2026-07-09 20:00:05 +0530

    Implement function to count sign changes in array

---
+++ catchSignChange.js
@@ -0,0 +1,13 @@
+function catchSignChange(arr) {
+  let count = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (
+      (arr[i - 1] < 0 && arr[i] >= 0) ||
+      (arr[i - 1] >= 0 && arr[i] < 0)
+    ) {
+      count++;
+    }
+  }
+
+  return count;
+}
