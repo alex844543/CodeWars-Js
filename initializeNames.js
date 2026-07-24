54921e9b88ed6a6888cf9622bd4875ec4a8a180c
Amritansh Roy <f23701837@gmail.com>
2026-07-24 21:26:43 +0530

    Add function to initialize names from a string

---
+++ initializeNames.js
@@ -0,0 +1,18 @@
+function initializeNames(name){
+
+let spit  = name.split(" ")
+let res = []
+
+for(let i = 0 ; i < spit.length ; i++){
+  if(i !== 0 && i !== spit.length-1){
+    let n = spit[i][0]
+    res.push(n+'.')
+  }else{
+    res.push(spit[i])
+  }
+}
+  
+  return res.join(" ")
+
+
+}
