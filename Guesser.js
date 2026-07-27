90d4f6063c66878f98cd5c2595447df813906b58
Amritansh Roy <f23701837@gmail.com>
2026-07-27 21:15:30 +0530

    Implement Guesser class for number guessing game

---
+++ Guesser.js
@@ -0,0 +1,22 @@
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+
+  guess(n) {
+    if (this.lives <= 0) {
+      
+      throw new Error("Already dead");
+    }
+
+    if (n === this.number) {
+      
+      return true;
+    }
+
+    this.lives--
+    
+    return false
+  }
+}
