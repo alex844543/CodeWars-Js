2d914911b418dd1bd587c9816da8ef4509064816
Amritansh Roy <f23701837@gmail.com>
2026-08-03 19:55:19 +0530

    Swap first and last elements in array

--- firstReverseTry.js
+++ firstReverseTry.js
@@ -1,11 +1,13 @@
 function firstReverseTry(arr) {
   
   if(arr.length === 0){
+    
     return []
   }
 
   let first = arr[0]
   let last = arr[arr.length-1]
+  
   
   arr[0] = last
   arr[arr.length -1] = first
